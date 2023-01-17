d1fce97ddf912bfd69ec9bb5ab48921feffa71d4
Jair Enrique Gómez Rodríguez <33232689+jairGomezRod@users.noreply.github.com>
2023-01-16 22:51:42 -0500

    :construction: Update console log

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -4,7 +4,7 @@ import {Text, TouchableOpacity} from 'react-native';
 const Button = ({content, color}) => {
   return (
     <TouchableOpacity 
-      onPress={() => {console.log("wenas")}}
+      onPress={() => {console.log("Sign Button Click")}}
       className={`inline-block px-7 py-3 my-5 bg-[${color}] rounded-full shadow-md`}
     >
       <Text className="text-center font-medium text-base leading-snug">{content}</Text>
