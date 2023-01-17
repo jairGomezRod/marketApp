d70af27a40f89b3660cb7915e9a0bd52f9a5e41c
Jair Enrique Gómez Rodríguez <33232689+jairGomezRod@users.noreply.github.com>
2023-01-16 20:05:35 -0500

    :sparkles: Button component creation

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+
+const Button = ({content, color}) => {
+  return (
+    <TouchableOpacity 
+      onPress={() => {console.log("wenas")}}
+      className={`inline-block px-7 py-3 my-5 bg-[${color}] rounded-full shadow-md`}
+    >
+      <Text className="text-center font-medium text-base leading-snug">{content}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default Button;
